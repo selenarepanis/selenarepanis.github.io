52336ae52eb05b35373aade64308b2b48f8fe4ee
selenarepanis <32667726+selenarepanis@users.noreply.github.com>
2018-04-22 19:48:49 +1000

    uploading fish

---
+++ 5_archive_class/assets/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+$('.b-red').click(function(){
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+
+  $('.item.red').addClass('show');
+  $('.item.red').removeClass('hide');
+});
+
+
+
+
+
+
+
+
+// dont remove this line
+
+});
---
+++ 6_archive/assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+//
+
+$('.menu').click(function(){
+$('.nav').addClass('active');
+  });
+
+
+
+/* do not write past this line*/
+
+});
