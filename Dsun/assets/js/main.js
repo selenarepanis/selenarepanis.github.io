579f79401c432e639bdae2ecb74cda65111a97cb
selenarepanis <32667726+selenarepanis@users.noreply.github.com>
2018-08-31 11:55:24 +1000

    :0

---
+++ Dsun/assets/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+// Hover in
+$('.navigation').mouseenter(function(){
+  $('.informationbutton').addClass('active');
+
+});
+
+// Hover out
+$('.informationbutton').mouseleave(function(){
+  $('.informationbutton').removeClass('active');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
